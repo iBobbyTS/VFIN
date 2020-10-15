ad42b24e6a6550363b2219ff909f786fdc165f67
iBobby <46138297+iBobbyTS@users.noreply.github.com>
2020-10-15 21:24:58 +0800

    Update interpolate.py

--- DAIN/interpolate.py
+++ DAIN/interpolate.py
@@ -9,6 +9,7 @@ import warnings
 
 
 def main(process_info):
+    start_time = time.time()
     warnings.filterwarnings("ignore", category=UserWarning)
     torch.backends.cudnn.benchmark = True
     
@@ -20,6 +21,7 @@ def main(process_info):
         filter_size=4,
         timestep=1 / process_info['sf'],
         training=False).cuda()
+    torch.cuda.empty_cache()
     model_path = process_info['model_path']
     
     pretrained_dict = torch.load(model_path)
@@ -44,14 +46,14 @@ def main(process_info):
     input_files = process_info['frames_to_process']
     loop_timer = []
     try:
-        start_time = time.time()
-
         X1_ori = torch.cuda.FloatTensor(numpy.load(f'{process_info["current_temp_file_path"]}/in/{input_files[0]}')['arr_0'])[:, :, :3].permute(2, 0, 1) / 255
+        torch.cuda.empty_cache()
         for _ in range(len(input_files) - 1):
             filename_frame_2 = f'{process_info["current_temp_file_path"]}/in/{input_files[_ + 1]}'
 
             X0 = X1_ori
             X1 = torch.cuda.FloatTensor(numpy.load(filename_frame_2)['arr_0'])[:, :, :3].permute(2, 0, 1) / 255
+            torch.cuda.empty_cache()
             X1_ori = X1
 
             assert (X0.size() == X1.size())
@@ -85,9 +87,12 @@ def main(process_info):
             X0 = Variable(torch.unsqueeze(X0, 0))
             X1 = Variable(torch.unsqueeze(X1, 0))
             X0 = pader(X0)
+#             torch.cuda.empty_cache()
             X1 = pader(X1)
+            torch.cuda.empty_cache()
 
             y_s, offset, filter = model(torch.stack((X0, X1), dim=0))
+            torch.cuda.empty_cache()
             y_ = y_s[process_info['save_which']]
 
             X0 = X0.data.cpu().numpy()
@@ -110,7 +115,8 @@ def main(process_info):
                 (1, 2, 0)) for filter_i in filter] if filter is not None else None
             X1 = numpy.transpose(255.0 * X1.clip(0, 1.0)[0, :, intPaddingTop:intPaddingTop + intHeight,
                                          intPaddingLeft: intPaddingLeft + intWidth], (1, 2, 0))
-
+            torch.cuda.empty_cache()
+            
             interpolated_frame_number = 0
             shutil.copy(f'{process_info["current_temp_file_path"]}/in/{input_files[_]}',
                         f'{process_info["current_temp_file_path"]}/out/{input_files[_].replace(".npz", "")}_{"0".zfill(sf_length)}.npz')
