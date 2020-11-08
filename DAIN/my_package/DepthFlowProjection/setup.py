0172d127afc67c6b835f5fa6133d65ae33ad5e64
iBobby <46138297+iBobbyTS@users.noreply.github.com>
2020-11-08 21:12:23 +0800

    Update setup.py

--- DAIN/my_package/DepthFlowProjection/setup.py
+++ DAIN/my_package/DepthFlowProjection/setup.py
@@ -1,19 +1,19 @@
-#!/usr/bin/env python3
 import os
+import json
+from setuptools import setup, find_packages
+
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 import torch
 
-from setuptools import setup, find_packages
-from torch.utils.cpp_extension import BuildExtension, CUDAExtension
-
-from compiler_args import nvcc_args, cxx_args
-
+with open('../../compiler_args.json') as f:
+    extra_compile_args = json.load(f)
 setup(
     name='depthflowprojection_cuda',
     ext_modules=[
         CUDAExtension('depthflowprojection_cuda', [
             'depthflowprojection_cuda.cc',
             'depthflowprojection_cuda_kernel.cu'
-        ], extra_compile_args={'cxx': cxx_args, 'nvcc': nvcc_args})
+        ], extra_compile_args=extra_compile_args)
     ],
     cmdclass={
         'build_ext': BuildExtension
