19a73162006d22f097b5920a64a492c8f42ff6b2
iBobby <46138297+iBobbyTS@users.noreply.github.com>
2020-11-12 08:59:09 +0800

    Update interpolator.py

--- SSM/interpolator.py
+++ SSM/interpolator.py
@@ -10,6 +10,8 @@ warnings.filterwarnings("ignore")
 
 
 class Interpolator:
+    torch.set_grad_enabled(False)
+
     def __init__(self, model_directory: str, sf: int, height: int, width: int, batch_size=1, **ssm):
         # sf
         self.sf = sf
@@ -66,41 +68,41 @@ class Interpolator:
         return torch.Tensor(numpy.transpose(numpy.array(out_frames), (0, 3, 1, 2))[:, ::-1].astype('float32') / 255)
 
     def interpolate(self, frames: list):
-        with torch.no_grad():
-            I0 = self.ndarray2tensor(frames[:-1])
-            I1 = self.ndarray2tensor(frames[1:])
-            flowOut = self.flowComp(torch.cat((I0, I1), dim=1))
-            F_0_1 = flowOut[:, :2, :, :]
-            F_1_0 = flowOut[:, 2:, :, :]
-            intermediate_frames = []  # Each item contains intermediate frames
-            for intermediateIndex in range(1, self.sf):
-                t = float(intermediateIndex) / self.sf
-                temp = -t * (1 - t)
-                fCoeff = [temp, t * t, (1 - t) * (1 - t), temp]
+        self.input_frames_tensor = self.ndarray2tensor(frames)
+        I0 = self.input_frames_tensor[:-1]
+        I1 = self.input_frames_tensor[1:]
+        flowOut = self.flowComp(torch.cat((I0, I1), dim=1))
+        F_0_1 = flowOut[:, :2, :, :]
+        F_1_0 = flowOut[:, 2:, :, :]
+        intermediate_frames = list(range(self.sf-1))  # Each item contains intermediate frames
+        for intermediateIndex in range(1, self.sf):
+            t = float(intermediateIndex) / self.sf
+            temp = -t * (1 - t)
+            fCoeff = [temp, t * t, (1 - t) * (1 - t), temp]
 
-                F_t_0 = fCoeff[0] * F_0_1 + fCoeff[1] * F_1_0
-                F_t_1 = fCoeff[2] * F_0_1 + fCoeff[3] * F_1_0
+            F_t_0 = fCoeff[0] * F_0_1 + fCoeff[1] * F_1_0
+            F_t_1 = fCoeff[2] * F_0_1 + fCoeff[3] * F_1_0
 
-                g_I0_F_t_0 = self.flowBackWarp(I0, F_t_0)
-                g_I1_F_t_1 = self.flowBackWarp(I1, F_t_1)
+            g_I0_F_t_0 = self.flowBackWarp(I0, F_t_0)
+            g_I1_F_t_1 = self.flowBackWarp(I1, F_t_1)
 
-                intrpOut = self.ArbTimeFlowIntrp(
-                    torch.cat((I0, I1, F_0_1, F_1_0, F_t_1, F_t_0, g_I1_F_t_1, g_I0_F_t_0), dim=1))
+            intrpOut = self.ArbTimeFlowIntrp(
+                torch.cat((I0, I1, F_0_1, F_1_0, F_t_1, F_t_0, g_I1_F_t_1, g_I0_F_t_0), dim=1))
 
-                F_t_0_f = intrpOut[:, :2, :, :] + F_t_0
-                F_t_1_f = intrpOut[:, 2:4, :, :] + F_t_1
-                V_t_0 = torch.sigmoid(intrpOut[:, 4:5, :, :])
-                V_t_1 = 1 - V_t_0
+            F_t_0_f = intrpOut[:, :2, :, :] + F_t_0
+            F_t_1_f = intrpOut[:, 2:4, :, :] + F_t_1
+            V_t_0 = torch.sigmoid(intrpOut[:, 4:5, :, :])
+            V_t_1 = 1 - V_t_0
 
-                g_I0_F_t_0_f = self.flowBackWarp(I0, F_t_0_f)
-                g_I1_F_t_1_f = self.flowBackWarp(I1, F_t_1_f)
+            g_I0_F_t_0_f = self.flowBackWarp(I0, F_t_0_f)
+            g_I1_F_t_1_f = self.flowBackWarp(I1, F_t_1_f)
 
-                wCoeff = [1 - t, t]
+            wCoeff = [1 - t, t]
 
-                Ft_p = (wCoeff[0] * V_t_0 * g_I0_F_t_0_f + wCoeff[1] * V_t_1 * g_I1_F_t_1_f) / (
-                        wCoeff[0] * V_t_0 + wCoeff[1] * V_t_1)
+            Ft_p = (wCoeff[0] * V_t_0 * g_I0_F_t_0_f + wCoeff[1] * V_t_1 * g_I1_F_t_1_f) / (
+                    wCoeff[0] * V_t_0 + wCoeff[1] * V_t_1)
 
-                # Save intermediate frame
-                # Ft_p contains batches of one intermediate frame
-                intermediate_frames.append(self.tensor2ndarray(Ft_p))
+            # Save intermediate frame
+            # Ft_p contains batches of one intermediate frame
+            intermediate_frames[intermediateIndex - 1] = self.tensor2ndarray(Ft_p)
         return intermediate_frames
