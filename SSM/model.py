24ff179b6aed4e953426cfa779a0f8b47c47272b
iBobby <46138297+iBobbyTS@users.noreply.github.com>
2020-12-24 13:00:06 +0800

    Update model.py

--- SSM/model.py
+++ SSM/model.py
@@ -24,6 +24,7 @@ class down(nn.Module):
         block.
     """
 
+
     def __init__(self, inChannels, outChannels, filterSize):
         """
         Parameters
@@ -39,11 +40,12 @@ class down(nn.Module):
                 a N x N filter.
         """
 
+
         super(down, self).__init__()
         # Initialize convolutional layers.
-        self.conv1 = nn.Conv2d(inChannels, outChannels, filterSize, stride=1, padding=int((filterSize - 1) / 2))
+        self.conv1 = nn.Conv2d(inChannels,  outChannels, filterSize, stride=1, padding=int((filterSize - 1) / 2))
         self.conv2 = nn.Conv2d(outChannels, outChannels, filterSize, stride=1, padding=int((filterSize - 1) / 2))
-
+           
     def forward(self, x):
         """
         Returns output tensor after passing input `x` to the neural network
@@ -60,15 +62,15 @@ class down(nn.Module):
                 output of the NN block.
         """
 
+
         # Average pooling with kernel size 2 (2 x 2).
         x = F.avg_pool2d(x, 2)
         # Convolution + Leaky ReLU
-        x = F.leaky_relu(self.conv1(x), negative_slope=0.1)
+        x = F.leaky_relu(self.conv1(x), negative_slope = 0.1)
         # Convolution + Leaky ReLU
-        x = F.leaky_relu(self.conv2(x), negative_slope=0.1)
+        x = F.leaky_relu(self.conv2(x), negative_slope = 0.1)
         return x
-
-
+    
 class up(nn.Module):
     """
     A class for creating neural network blocks containing layers:
@@ -86,6 +88,7 @@ class up(nn.Module):
         block.
     """
 
+
     def __init__(self, inChannels, outChannels):
         """
         Parameters
@@ -98,12 +101,13 @@ class up(nn.Module):
                 the second convolutional layer.
         """
 
+        
         super(up, self).__init__()
         # Initialize convolutional layers.
-        self.conv1 = nn.Conv2d(inChannels, outChannels, 3, stride=1, padding=1)
+        self.conv1 = nn.Conv2d(inChannels,  outChannels, 3, stride=1, padding=1)
         # (2 * outChannels) is used for accommodating skip connection.
         self.conv2 = nn.Conv2d(2 * outChannels, outChannels, 3, stride=1, padding=1)
-
+           
     def forward(self, x, skpCn):
         """
         Returns output tensor after passing input `x` to the neural network
@@ -123,12 +127,13 @@ class up(nn.Module):
         """
 
         # Bilinear interpolation with scaling 2.
-        x = F.interpolate(x, scale_factor=2, mode='bilinear')
+        x = F.interpolate(x, scale_factor=2, mode='bilinear', align_corners=True)
         # Convolution + Leaky ReLU
-        x = F.leaky_relu(self.conv1(x), negative_slope=0.1)
+        x = F.leaky_relu(self.conv1(x), negative_slope = 0.1)
         # Convolution + Leaky ReLU on (`x`, `skpCn`)
-        x = F.leaky_relu(self.conv2(torch.cat((x, skpCn), 1)), negative_slope=0.1)
+        x = F.leaky_relu(self.conv2(torch.cat((x, skpCn), 1)), negative_slope = 0.1)
         return x
+
 
 
 class UNet(nn.Module):
@@ -145,6 +150,7 @@ class UNet(nn.Module):
         block.
     """
 
+
     def __init__(self, inChannels, outChannels):
         """
         Parameters
@@ -155,6 +161,7 @@ class UNet(nn.Module):
                 number of output channels for the UNet.
         """
 
+        
         super(UNet, self).__init__()
         # Initialize neural network blocks.
         self.conv1 = nn.Conv2d(inChannels, 32, 7, stride=1, padding=3)
@@ -164,13 +171,13 @@ class UNet(nn.Module):
         self.down3 = down(128, 256, 3)
         self.down4 = down(256, 512, 3)
         self.down5 = down(512, 512, 3)
-        self.up1 = up(512, 512)
-        self.up2 = up(512, 256)
-        self.up3 = up(256, 128)
-        self.up4 = up(128, 64)
-        self.up5 = up(64, 32)
+        self.up1   = up(512, 512)
+        self.up2   = up(512, 256)
+        self.up3   = up(256, 128)
+        self.up4   = up(128, 64)
+        self.up5   = up(64, 32)
         self.conv3 = nn.Conv2d(32, outChannels, 3, stride=1, padding=1)
-
+        
     def forward(self, x):
         """
         Returns output tensor after passing input `x` to the neural network.
@@ -186,19 +193,20 @@ class UNet(nn.Module):
                 output of the UNet.
         """
 
-        x = F.leaky_relu(self.conv1(x), negative_slope=0.1)
-        s1 = F.leaky_relu(self.conv2(x), negative_slope=0.1)
+
+        x  = F.leaky_relu(self.conv1(x), negative_slope = 0.1)
+        s1 = F.leaky_relu(self.conv2(x), negative_slope = 0.1)
         s2 = self.down1(s1)
         s3 = self.down2(s2)
         s4 = self.down3(s3)
         s5 = self.down4(s4)
-        x = self.down5(s5)
-        x = self.up1(x, s5)
-        x = self.up2(x, s4)
-        x = self.up3(x, s3)
-        x = self.up4(x, s2)
-        x = self.up5(x, s1)
-        x = F.leaky_relu(self.conv3(x), negative_slope=0.1)
+        x  = self.down5(s5)
+        x  = self.up1(x, s5)
+        x  = self.up2(x, s4)
+        x  = self.up3(x, s3)
+        x  = self.up4(x, s2)
+        x  = self.up5(x, s1)
+        x  = F.leaky_relu(self.conv3(x), negative_slope = 0.1)
         return x
 
 
@@ -220,6 +228,7 @@ class backWarp(nn.Module):
         block.
     """
 
+
     def __init__(self, W, H, device):
         """
         Parameters
@@ -232,6 +241,7 @@ class backWarp(nn.Module):
                 computation device (cpu/cuda). 
         """
 
+
         super(backWarp, self).__init__()
         # create a grid
         gridX, gridY = np.meshgrid(np.arange(W), np.arange(H))
@@ -239,7 +249,7 @@ class backWarp(nn.Module):
         self.H = H
         self.gridX = torch.tensor(gridX, requires_grad=False, device=device)
         self.gridY = torch.tensor(gridY, requires_grad=False, device=device)
-
+        
     def forward(self, img, flow):
         """
         Returns output tensor after passing input `img` and `flow` to the backwarping
@@ -259,18 +269,19 @@ class backWarp(nn.Module):
                 frame I0.
         """
 
+
         # Extract horizontal and vertical flows.
         u = flow[:, 0, :, :]
         v = flow[:, 1, :, :]
         x = self.gridX.unsqueeze(0).expand_as(u).float() + u
         y = self.gridY.unsqueeze(0).expand_as(v).float() + v
         # range -1 to 1
-        x = 2 * (x / self.W - 0.5)
-        y = 2 * (y / self.H - 0.5)
+        x = 2*(x/self.W - 0.5)
+        y = 2*(y/self.H - 0.5)
         # stacking X and Y
-        grid = torch.stack((x, y), dim=3)
+        grid = torch.stack((x,y), dim=3)
         # Sample pixels using bilinear interpolation.
-        imgOut = torch.nn.functional.grid_sample(img, grid)
+        imgOut = torch.nn.functional.grid_sample(img, grid, align_corners=True)
         return imgOut
 
 
@@ -278,8 +289,7 @@ class backWarp(nn.Module):
 # reference frames I0 and I1. 
 t = np.linspace(0.125, 0.875, 7)
 
-
-def getFlowCoeff(indices, device):
+def getFlowCoeff (indices, device):
     """
     Gets flow coefficients used for calculating intermediate optical
     flows from optical flows between I0 and I1: F_0_1 and F_1_0.
@@ -307,19 +317,15 @@ def getFlowCoeff(indices, device):
             coefficients C00, C01, C10, C11.
     """
 
+
     # Convert indices tensor to numpy array
     ind = indices.detach().numpy()
     C11 = C00 = - (1 - (t[ind])) * (t[ind])
     C01 = (t[ind]) * (t[ind])
     C10 = (1 - (t[ind])) * (1 - (t[ind]))
-    return torch.Tensor(C00)[None, None, None, :].permute(3, 0, 1, 2).to(device), torch.Tensor(C01)[None, None, None,
-                                                                                  :].permute(3, 0, 1, 2).to(
-        device), torch.Tensor(C10)[None, None, None, :].permute(3, 0, 1, 2).to(device), torch.Tensor(C11)[None, None,
-                                                                                        None, :].permute(3, 0, 1, 2).to(
-        device)
+    return torch.Tensor(C00)[None, None, None, :].permute(3, 0, 1, 2).to(device), torch.Tensor(C01)[None, None, None, :].permute(3, 0, 1, 2).to(device), torch.Tensor(C10)[None, None, None, :].permute(3, 0, 1, 2).to(device), torch.Tensor(C11)[None, None, None, :].permute(3, 0, 1, 2).to(device)
 
-
-def getWarpCoeff(indices, device):
+def getWarpCoeff (indices, device):
     """
     Gets coefficients used for calculating final intermediate 
     frame `It_gen` from backwarped images using flows F_t_0 and F_t_1.
@@ -347,9 +353,9 @@ def getWarpCoeff(indices, device):
             coefficients C0 and C1.
     """
 
+
     # Convert indices tensor to numpy array
     ind = indices.detach().numpy()
     C0 = 1 - t[ind]
     C1 = t[ind]
-    return torch.Tensor(C0)[None, None, None, :].permute(3, 0, 1, 2).to(device), torch.Tensor(C1)[None, None, None,
-                                                                                 :].permute(3, 0, 1, 2).to(device)
+    return torch.Tensor(C0)[None, None, None, :].permute(3, 0, 1, 2).to(device), torch.Tensor(C1)[None, None, None, :].permute(3, 0, 1, 2).to(device)
